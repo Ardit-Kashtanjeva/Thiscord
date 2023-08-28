749b89da3cd70868a0746fb5965b3128f0119c26
Ardit-Kashtanjeva <ardit.kashtanjeva@cmiag.ch>
2023-08-28 11:21:40 +0200

    ein paar Steuerelemente hinzugefügt
    ClientIP Lbl und Txtbox hinzugefügt. Messagehistory Txtbox hinzugefügt.

--- LNM Exclusive/LNM Exclusive/Form1.Designer.cs
+++ LNM Exclusive/LNM Exclusive/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LblClientIP = new System.Windows.Forms.Label();
+            this.TxtClientIP = new System.Windows.Forms.TextBox();
+            this.TxtMessageHistory = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LblClientIP
+            // 
+            this.LblClientIP.AutoSize = true;
+            this.LblClientIP.Location = new System.Drawing.Point(49, 46);
+            this.LblClientIP.Name = "LblClientIP";
+            this.LblClientIP.Size = new System.Drawing.Size(115, 32);
+            this.LblClientIP.TabIndex = 0;
+            this.LblClientIP.Text = "Your IP:";
+            // 
+            // TxtClientIP
+            // 
+            this.TxtClientIP.Location = new System.Drawing.Point(170, 46);
+            this.TxtClientIP.Name = "TxtClientIP";
+            this.TxtClientIP.ReadOnly = true;
+            this.TxtClientIP.Size = new System.Drawing.Size(253, 38);
+            this.TxtClientIP.TabIndex = 1;
+            // 
+            // TxtMessageHistory
+            // 
+            this.TxtMessageHistory.Location = new System.Drawing.Point(1186, 181);
+            this.TxtMessageHistory.Name = "TxtMessageHistory";
+            this.TxtMessageHistory.Size = new System.Drawing.Size(500, 38);
+            this.TxtMessageHistory.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1789, 1058);
+            this.Controls.Add(this.TxtMessageHistory);
+            this.Controls.Add(this.TxtClientIP);
+            this.Controls.Add(this.LblClientIP);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LblClientIP;
+        private System.Windows.Forms.TextBox TxtClientIP;
+        private System.Windows.Forms.TextBox TxtMessageHistory;
     }
 }
 
