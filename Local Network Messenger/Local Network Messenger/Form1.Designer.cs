f53f9d1128e142ea03d79477addc6327f25005f6
Ardit-Kashtanjeva <ardit.kashtanjeva@cmiag.ch>
2023-09-25 10:12:20 +0200

    Fertig

--- Local Network Messenger/Local Network Messenger/Form1.Designer.cs
+++ Local Network Messenger/Local Network Messenger/Form1.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            TxtMessageHistory = new TextBox();
+            LblYourIp = new Label();
+            TxtUserIp = new TextBox();
+            TxtMessageBox = new TextBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            CmdSend = new Button();
+            CmdConnextToTarget = new Button();
+            SuspendLayout();
+            // 
+            // TxtMessageHistory
+            // 
+            TxtMessageHistory.Location = new Point(978, 182);
+            TxtMessageHistory.MaximumSize = new Size(250, 150);
+            TxtMessageHistory.MinimumSize = new Size(450, 600);
+            TxtMessageHistory.Multiline = true;
+            TxtMessageHistory.Name = "TxtMessageHistory";
+            TxtMessageHistory.Size = new Size(450, 600);
+            TxtMessageHistory.TabIndex = 0;
+            // 
+            // LblYourIp
+            // 
+            LblYourIp.AutoSize = true;
+            LblYourIp.Location = new Point(36, 27);
+            LblYourIp.Name = "LblYourIp";
+            LblYourIp.Size = new Size(117, 41);
+            LblYourIp.TabIndex = 1;
+            LblYourIp.Text = "Your IP:";
+            // 
+            // TxtUserIp
+            // 
+            TxtUserIp.Location = new Point(173, 27);
+            TxtUserIp.Name = "TxtUserIp";
+            TxtUserIp.Size = new Size(529, 47);
+            TxtUserIp.TabIndex = 2;
+            // 
+            // TxtMessageBox
+            // 
+            TxtMessageBox.Location = new Point(978, 788);
+            TxtMessageBox.MinimumSize = new Size(350, 100);
+            TxtMessageBox.Name = "TxtMessageBox";
+            TxtMessageBox.Size = new Size(350, 100);
+            TxtMessageBox.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(877, 27);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(529, 47);
+            textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(716, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(155, 41);
+            label1.TabIndex = 5;
+            label1.Text = "Friends Ip:";
+            // 
+            // CmdSend
+            // 
+            CmdSend.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            CmdSend.BackgroundImage = (Image)resources.GetObject("CmdSend.BackgroundImage");
+            CmdSend.BackgroundImageLayout = ImageLayout.Stretch;
+            CmdSend.Location = new Point(1328, 788);
+            CmdSend.MinimumSize = new Size(100, 100);
+            CmdSend.Name = "CmdSend";
+            CmdSend.Size = new Size(100, 100);
+            CmdSend.TabIndex = 6;
+            CmdSend.UseMnemonic = false;
+            CmdSend.UseVisualStyleBackColor = true;
+            CmdSend.Click += SendMessageButton_Click;
+            // 
+            // CmdConnextToTarget
+            // 
+            CmdConnextToTarget.Location = new Point(1044, 80);
+            CmdConnextToTarget.Name = "CmdConnextToTarget";
+            CmdConnextToTarget.Size = new Size(188, 58);
+            CmdConnextToTarget.TabIndex = 7;
+            CmdConnextToTarget.Text = "Connect";
+            CmdConnextToTarget.UseVisualStyleBackColor = true;
+            CmdConnextToTarget.Click += ConnectToTargetButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1455, 917);
+            Controls.Add(CmdConnextToTarget);
+            Controls.Add(CmdSend);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(TxtMessageBox);
+            Controls.Add(TxtUserIp);
+            Controls.Add(LblYourIp);
+            Controls.Add(TxtMessageHistory);
+            Name = "Form1";
+            Text = "LNM";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox TxtMessageHistory;
+        private Label LblYourIp;
+        private TextBox TxtUserIp;
+        private TextBox TxtMessageBox;
+        private TextBox textBox1;
+        private Label label1;
+        private Button CmdSend;
+        private Button CmdConnextToTarget;
     }
 }
--- Local Network Messenger/Local Network Messenger/Form1.cs
+++ Local Network Messenger/Local Network Messenger/Form1.cs
@@ -1,10 +1,100 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
 namespace Local_Network_Messenger
 {
     public partial class Form1 : Form
     {
+        private TcpListener tcpListener;
+        private TcpClient tcpClient;
+
         public Form1()
         {
             InitializeComponent();
+            string hostName = Dns.GetHostName();
+            IPHostEntry localHost = Dns.GetHostEntry(hostName);
+            foreach (IPAddress address in localHost.AddressList)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    TxtUserIp.Text = address.ToString();
+                    break;
+                }
+            }
+
+            tcpListener = new TcpListener(IPAddress.Any, 12345);
+            tcpListener.Start();
+            ListenForClients();
         }
+
+        private async void ListenForClients()
+        {
+            while (true)
+            {
+                TcpClient client = await tcpListener.AcceptTcpClientAsync();
+                tcpClient = client;
+                Task.Run(() => HandleTargetMessages());
+            }
+        }
+
+        private void HandleTargetMessages()
+        {
+            NetworkStream stream = tcpClient.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                DisplayReceivedMessage(receivedMessage);
+            }
+        }
+
+        private void DisplayReceivedMessage(string message)
+        {
+            TxtMessageHistory.AppendText($"[Target]: {message}\n");
+        }
+
+        private void ConnectToTargetButton_Click(object sender, EventArgs e)
+        {
+            string targetIpAddress = textBox1.Text;
+            IPAddress targetAddress;
+
+            if (IPAddress.TryParse(targetIpAddress, out targetAddress))
+            {
+                tcpClient = new TcpClient();
+                tcpClient.Connect(targetAddress, 12345);
+                Task.Run(() => HandleTargetMessages());
+            }
+            else
+            {
+                MessageBox.Show("Invalid target IP address.");
+            }
+        }
+
+        private void SendMessageButton_Click(object sender, EventArgs e)
+        {
+            if (tcpClient != null && tcpClient.Connected)
+            {
+                string messageToSend = TxtMessageBox.Text;
+                byte[] data = Encoding.UTF8.GetBytes(messageToSend);
+                NetworkStream stream = tcpClient.GetStream();
+                stream.Write(data, 0, data.Length);
+                stream.Flush();
+                DisplaySentMessage(messageToSend);
+            }
+        }
+
+        private void DisplaySentMessage(string message)
+        {
+            TxtMessageHistory.AppendText($"[You]: {message}\n");
+        }
+
+
     }
 }
